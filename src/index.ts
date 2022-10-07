0d47bdf01096e8d9a089a9e79dee8da47698dc8b
Adalberto Rua <adalbertor@solin.com.co>
2022-10-07 04:25:04 -0500

    se agrego el worker

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,24 @@ export { ArkeosMonacoEditor } from "./components/arkeos-monaco-editor.xtag";
 
 declare var xtag: any;
 
+self.MonacoEnvironment = {
+	getWorkerUrl: function (moduleId, label) {
+		if (label === 'json') {
+			return './json.worker.bundle.js';
+		}
+		if (label === 'css' || label === 'scss' || label === 'less') {
+			return './css.worker.bundle.js';
+		}
+		if (label === 'html' || label === 'handlebars' || label === 'razor') {
+			return './html.worker.bundle.js';
+		}
+		if (label === 'typescript' || label === 'javascript') {
+			return './ts.worker.bundle.js';
+		}
+		return './editor.worker.bundle.js';
+	}
+};
+
 function registerComponent(webComponent: string, newComponent: any) {
     let component: any = customElements.get(webComponent);
     if(component) {
